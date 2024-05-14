6eafc4a85cfb8f296be5b2eb44703badece5be7a
kevvj <kevinjvillalba774+1@gmail.com>
2024-05-13 22:53:33 -0500

    asdasd

--- src/app/page.js
+++ src/app/page.js
@@ -1,27 +1,33 @@
 "use client"
-import {useState} from 'react'
-import Aform from "./components/askForm"
+import { useState } from 'react'
 import ButtonHeader from "./components/ButtonHeader"
 import Header from "./components/Header"
 
 
- function HomePage() {
-  
-const [info, setInfo] = useState('')
+function HomePage() {
 
-const HandleClick = () =>{
-  event.preventDefault();
-  setInfo('holp')
-  console.log(info)
-}
-  
+  const [info, setInfo] = useState('')
+
+  const handleSumit = () => {
+    event.preventDefault();
+   
+  }
+
+  const handleChange = (event) => {
+    setInfo(event.target.value);
+  }
+
   return (
     <div>
-          <Header /> 
-          <Aform fun={HandleClick}/>
-
+      <Header />
+      <form className="mx-auto text-center flex flex-wrap justify-center align-items-center w-96 gap-6" onSubmit={handleSumit}>
+        <p className="text-white" type="text" >Holp</p>
+        <input placeholder="Texto" className="h-8 w-96" onChange={handleChange}></input>
+        <button className="bg-white" >boton</button>
+      </form>
+      <p className="text-white" >{info}</p>
     </div>
-  
+
   )
 }
 
