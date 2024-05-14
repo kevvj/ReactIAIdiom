43c8a35b503688202ab922d6c3e36a1352248a6f
kevvj <kevinjvillalba774+1@gmail.com>
2024-05-13 21:28:28 -0500

    sd

--- src/app/components/askForm.js
+++ src/app/components/askForm.js
@@ -1,9 +1,13 @@
-const Aform = () =>{
+
+const Aform = ({fun}) =>{
+  const a = ()=>{
+
+  }
     return (
     <form className="mx-auto text-center flex flex-wrap justify-center align-items-center w-96 gap-6">
     <p className="text-white" type="text" >Holp</p>
     <input placeholder="Texto" className="h-8 w-96"></input>
-    <button className="bg-white">boton</button>
+    <button className="bg-white" onClick={fun}>boton</button>
   </form>
   )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,25 @@
+"use client"
+import {useState} from 'react'
 import Aform from "./components/askForm"
 import ButtonHeader from "./components/ButtonHeader"
 import Header from "./components/Header"
 
 
  function HomePage() {
+  
+const [info, setInfo] = useState('')
+
+const HandleClick = () =>{
+  event.preventDefault();
+  setInfo('holp')
+  console.log(info)
+}
+  
   return (
     <div>
           <Header /> 
-          <Aform></Aform>
+          <Aform fun={HandleClick}/>
+
     </div>
   
   )
