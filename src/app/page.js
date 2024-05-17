1a6a306b20af5857fc97a7dd5ac5f5c5473f4714
kevvj <kevinjvillalba774+1@gmail.com>
2024-05-16 22:57:59 -0500

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -6,28 +6,35 @@ import Header from "./components/Header"
 
 function HomePage() {
 
-  const [info, setInfo] = useState('')
-  const [numero, setNumero] = useState(0)
+  const [textinput, setTextinput] = useState('')
+  const [textResponse, setTextResponse] = useState([])
 
   const handleSumit = () => {
     event.preventDefault()
-    setNumero(numero+1)
+    setTextResponse([...textResponse, textinput])
+    setTextinput('')
   }
 
   const handleChange = (event) => {
-    setInfo(event.target.value)
+    setTextinput(event.target.value)
   }
 
   return (
     <div>
       <Header />
       <form className="mx-auto text-center flex flex-wrap justify-center align-items-center w-96 gap-6" onSubmit={handleSumit}>
+
         <p className="text-white" type="text" >Holp</p>
+
         <input placeholder="Texto" className="h-8 w-96" onChange={handleChange}></input>
+
         <button className="bg-white" >boton</button>
+
       </form>
-      <p className="text-white" >{info}</p>
-      <p className="text-white" >{numero}</p>
+
+      {textResponse.map((TR, index) => (
+          <p className="text-white" key={index}>{TR}</p>
+        ))}
     </div>
 
   )
